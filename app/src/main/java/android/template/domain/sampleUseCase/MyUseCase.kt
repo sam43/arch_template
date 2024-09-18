6cc543342e34a129cc462153e651fa8f7b4b28e9
sadat.sayem <sadat.sayem@rakuten.com>
2024-09-18 12:52:38 +0900

    feature/core-layer-upgrade - fix and run "core" module

--- app/src/main/java/android/template/domain/sampleUseCase/MyUseCase.kt
+++ app/src/main/java/android/template/domain/sampleUseCase/MyUseCase.kt
@@ -3,7 +3,7 @@ package android.template.domain.sampleUseCase
 import javax.inject.Inject
 
 class MyUseCase @Inject constructor() {
-	suspend operator fun invoke() {
+	operator fun invoke() {
 		// ... do your operation
 	}
 }
